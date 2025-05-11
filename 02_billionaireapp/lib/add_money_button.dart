4f9a29c092bd79ec6fe6b78722b87dd02648d428
himanshu7437 <himanshujangra368@gmail.com>
2025-05-11 16:22:45 +0530

    add functionality for managing balance with AddMoneyButton and BalanceView components; update AndroidManifest and add dependencies

---
+++ 02_billionaireapp/lib/add_money_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AddMoneyButton extends StatelessWidget {
+
+  void Function () addMoneyFunction;
+
+  AddMoneyButton({super.key, required this.addMoneyFunction});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+
+                    backgroundColor: Colors.red[700],
+                    minimumSize: Size(double.infinity, double.infinity),
+                  ),
+                  onPressed: addMoneyFunction,
+                  child: Text('Add Money'),
+                ),
+              );
+  }
+}
--- 02_billionaireapp/lib/main.dart
+++ 02_billionaireapp/lib/main.dart
@@ -1,19 +1,71 @@
+import 'package:billionaireapp/add_money_button.dart';
+import 'package:billionaireapp/balance_view.dart';
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  double balance = 0;
+
+  void addMoney() async {
+    setState(() {
+      balance = balance + 500;
+    });
+
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('balance', balance);
+  }
+
+  @override
+  void initState() {
+    
+    loadBalance();
+    super.initState();
+  }
+
+  void loadBalance() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    setState(() {
+      balance = prefs.getDouble('balance') ?? 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Billionaire App!"),
+          centerTitle: true,
+          backgroundColor: Colors.grey[800],
+          title: Text('Billionaire App!'),
+        ),
+
+        body: Container(
+          padding: EdgeInsets.all(20),
+          color: Colors.blueGrey[700],
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              BalanceView(balance: balance,),
+              AddMoneyButton(addMoneyFunction: addMoney,),
+            ],
+          ),
         ),
       ),
     );
